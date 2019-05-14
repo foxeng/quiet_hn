194b3da7adf52e6cbd47200e8164c1d0fa71586f
Fotis Xenakis <foxen@windowslive.com>
2019-05-14 16:45:23 +0300

    Add concurrency

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"sort"
 	"strings"
 	"time"
 
-	"github.com/gophercises/quiet_hn/hn"
+	"github.com/foxeng/quiet_hn/hn"
 )
 
 func main() {
@@ -37,20 +38,32 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 			http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
 			return
 		}
-		var stories []item
-		for _, id := range ids {
-			hnItem, err := client.GetItem(id)
-			if err != nil {
-				continue
-			}
-			item := parseHNItem(hnItem)
-			if isStoryLink(item) {
-				stories = append(stories, item)
-				if len(stories) >= numStories {
-					break
-				}
+
+		// An approach with zero extra work (no prefetching):
+		// Spawn numStories concurrent fetches
+		// For each one which returns not-a-story, spawn another fetch
+		// To order the items (since we cannot rely on the presence of item.Score), we
+		// channel the index (in ids) of each item back and forth in the goroutines
+		storiesm := make(map[int]*item)
+		var rank int
+		c := make(chan *item)
+		for rank = 0; rank < numStories; rank++ {
+			go fetchItem(&client, rank, ids[rank], c)
+		}
+
+		for len(storiesm) < numStories {
+			it := <-c
+			if it == nil {
+				go fetchItem(&client, rank, ids[rank], c)
+				rank++
+			} else {
+				storiesm[it.rank] = it
 			}
 		}
+		// TODO: Close c?
+
+		stories := mapToSlice(storiesm)
+
 		data := templateData{
 			Stories: stories,
 			Time:    time.Now().Sub(start),
@@ -61,6 +74,34 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 			return
 		}
 	})
+}
+
+func mapToSlice(m map[int]*item) []item {
+	var keys []int
+	for k, _ := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	items := make([]item, len(m))
+	for i, k := range keys {
+		items[i] = *m[k]
+	}
+	return items
+}
+
+func fetchItem(client *hn.Client, rank int, id int, out chan<- *item) {
+	hnItem, err := client.GetItem(id)
+	if err != nil {
+		out <- nil
+		return
+	}
+	it := parseHNItem(hnItem)
+	if !isStoryLink(it) {
+		out <- nil
+		return
+	}
+	it.rank = rank
+	out <- &it // TODO: Is this ok or does it need to be allocated with new()?
 }
 
 func isStoryLink(item item) bool {
@@ -76,10 +117,10 @@ func parseHNItem(hnItem hn.Item) item {
 	return ret
 }
 
-// item is the same as the hn.Item, but adds the Host field
 type item struct {
 	hn.Item
 	Host string
+	rank int
 }
 
 type templateData struct {
